92255659c8435a194a03d5efd0bb808c5b00691e
ricardoggz <ricardo_ggzicm@outlook.com>
2021-12-15 15:38:34 -0600

    hero terminado

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,11 @@
 import { NavBar } from "../components/navBar/NavBar";
+import {Hero} from "../components/hero/Hero";
 
 function App() {
   return (
     <>
       <NavBar />
+      <Hero />
       
     </>
   );
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,11 +2,22 @@ import React from "react";
 
 /*styles*/
 import "./hero.css";
+//components
+import { Button } from "../button/Button";
 
 export const Hero = () => {
   return (
     <section className="hero-container">
-      <p>Hola</p>
+      <div className="hero-title">
+        <div className="flex-container">
+          <h1>
+            Aprende desarrollo web, marketing online, diseño digital y más...
+          </h1>
+          <div className="button-title">
+            <Button title="Comenzar a aprender" />
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
