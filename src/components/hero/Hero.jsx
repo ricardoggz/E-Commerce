048273a971532c19b18c689138dceaaa0c437516
ricardoggz <ricardo_ggzicm@outlook.com>
2021-12-14 19:32:06 -0600

    home pendiente

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+/*styles*/
+import "./hero.css";
+
+export const Hero = () => {
+  return (
+    <section className="hero-container">
+      <p>Hola</p>
+    </section>
+  );
+};
