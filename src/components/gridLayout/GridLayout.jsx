d898c1da06fc4017ae20fc89f00403572a837924
ricardoggz <ricardo_ggzicm@outlook.com>
2021-12-18 16:22:37 -0600

    layout mejorado para props

--- src/components/gridLayout/GridLayout.jsx
+++ src/components/gridLayout/GridLayout.jsx
@@ -2,10 +2,6 @@ import React from "react";
 //styles
 import "./gridLayout.css";
 
-export const GridLayout = (props) => {
-  return (
-    <div className="grid-layout flex-container">
-      {props.children};
-    </div>
-  )
+export const GridLayout = ({ children }) => {
+  return <div className="grid-layout flex-container">{children};</div>;
 };
